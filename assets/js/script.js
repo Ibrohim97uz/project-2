7fb63940fd89aef95b019cf0dd3abba05612319d
Ibrohim Fattohov <ibrohim97uz@gmail.com>
2021-11-26 04:19:12 +0500

    university of utah

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector(".burger_menu");
+var list = document.querySelector(".nav_list_burger");
+menu.addEventListener("click", function () {
+  menu.classList.toggle("transform");
+  list.classList.toggle("show");
+});
+
+//create function for get random color
+
+
+
+
